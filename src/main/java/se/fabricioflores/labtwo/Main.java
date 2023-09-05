7100660525618fb6972feffc9cdfc37a0fff6b39
Fabrithegreat <fabrixio.rodrigo@gmail.com>
2023-09-05 14:58:54 +0200

    uppgift 3 & made other few changes

--- src/main/java/se/fabricioflores/labtwo/Main.java
+++ src/main/java/se/fabricioflores/labtwo/Main.java
@@ -1,7 +1,6 @@
 package se.fabricioflores.labtwo;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +8,7 @@ public class Main {
         Shape circle2 = Shape.createShape(12);
         Shape rectangle1= Shape.createShape(5, 2.2);
         Shape rectangle2 = Shape.createShape(10, 3.2);
+        Shape rectangle3 = Shape.createShape(10, 3.2);
 
         List<Shape> shapes = new ArrayList<>();
 
@@ -17,11 +17,22 @@ public class Main {
         shapes.add(rectangle1);
         shapes.add(rectangle2);
 
-        shapes.sort(null);
+        shapes.sort(null); // Listan sorteras baserad på våran comparable
 
         System.out.println("\n📐 Shape list sorted by area\n");
         for (Shape shape: shapes) {
             System.out.println(shapes.indexOf(shape) + ") " + shape.getArea());
+        }
+
+        Set<Shape> shapeSet = new HashSet<>();
+        shapeSet.add(circle1);
+        shapeSet.add(rectangle2);
+        shapeSet.add(rectangle3); // Kommer inte läggas till för det är en kopia av rectangle2
+
+        System.out.println("\n📐 Shape list with unique shapes\n");
+
+        for(Shape shape : shapeSet) {
+            System.out.println("- " + shape.getArea());
         }
     }
 }
@@ -43,6 +54,11 @@ abstract class Shape implements Comparable<Shape> {
     public int compareTo(Shape otherShape) {
         return Double.compare(this.getArea(), otherShape.getArea());
     }
+
+    //    Metoden compareTo returnerar en int
+    //    Negativt tal om det aktuella objektets area är mindre än otherShape.
+    //    Positivt tal om det aktuella objektets area är större än otherShape.
+    //    Noll om båda objekts area är lika.
 }
 
 class Rectangle extends Shape {
@@ -79,6 +95,20 @@ class Rectangle extends Shape {
     public double getPerimeter() {
         return (width + height) * 2;
     }
+
+    // Equals & Hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Double.compare(width, rectangle.width) == 0 && Double.compare(height, rectangle.height) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(width, height);
+    }
 }
 
 class Circle extends Shape {
@@ -104,5 +134,19 @@ class Circle extends Shape {
     @Override
     public double getPerimeter() {
         return 2 * Math.PI * radius;
+    }
+
+    // Equals & Hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Circle circle = (Circle) o;
+        return Double.compare(radius, circle.radius) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
     }
 }
